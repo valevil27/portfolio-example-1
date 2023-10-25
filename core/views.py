ebc01a108719caca485d053c4f9d7ecf25774625
valero <evalerovilella@gmail.com>
2023-10-25 23:37:17 +0200

    added about view

--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("<h1> Mi web personal </h1><h2>Portada</h2>")
+
+def about(request):
+    return HttpResponse("<h1> Mi web personal </h1><h2>Acerca de</h2>")
