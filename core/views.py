82d17652367950a0b82d6c7960ab869dfa8bf521
valero <evalerovilella@gmail.com>
2023-10-25 23:52:58 +0200

    added portfolio and contact views at core

--- core/views.py
+++ core/views.py
@@ -5,18 +5,52 @@ html_base = """
 <ul>
     <li><a href="/">Portada</a></li>
     <li><a href="/about-me/">Acerca De</a></li>
+    <li><a href="/portfolio/">Portfolio</a></li>
+    <li><a href="/contact/">Contacto</a></li>
 </ul>
 """
 
+
 # Create your views here.
 def home(request):
-    return HttpResponse(html_base + """
+    return HttpResponse(
+        html_base
+        + """
                         <h2>Portada</h2>
                         <p>Esto es la portada</p>
-                        """)
+                        """
+    )
+
 
 def about(request):
-    return HttpResponse(html_base + """
+    return HttpResponse(
+        html_base
+        + """
                         <h2>Acerca de</h2>
                         <p>Me llamo Eduardo y soy programador</p>
-                        """)
+                        """
+    )
+
+
+def portfolio(request):
+    return HttpResponse(
+        html_base
+        + """
+                        <h2>Portfolio</h2>
+                        <p>This is my portfolio with several beautiful projects.</p>                        
+                        """
+    )
+
+
+def contact(request):
+    return HttpResponse(
+        html_base
+        + """
+                        <h2>Portfolio</h2>
+                        <p>Contact me:</p>                        
+                        <ul>
+                            <li>Email: evalerovilella@gmail.com</li>
+                            <li>Instagram: @valevil96</li>
+                            <li>GitHub: <a href="https://www.github.com/Swamptk">Swamptk</a></li>
+                        """
+    )
