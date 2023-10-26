b45b35102d278657374f5ef11ce2632b37d1adbb
valero <evalerovilella@gmail.com>
2023-10-26 10:13:01 +0200

    added core to settings and base templates

--- core/views.py
+++ core/views.py
@@ -13,14 +13,7 @@ html_base = """
 
 # Create your views here.
 def home(request):
-    return HttpResponse(
-        html_base
-        + """
-                        <h2>Portada</h2>
-                        <p>Esto es la portada</p>
-                        """
-    )
-
+    return render(request, "core/home.html")
 
 def about(request):
     return HttpResponse(
